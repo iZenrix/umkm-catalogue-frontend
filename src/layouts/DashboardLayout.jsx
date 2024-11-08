db245a87c38b82750e2a4b64c24b51ef85bec1a2
QousulHaq <aqousulhaq@gmail.com>
2024-11-08 10:31:35 +0700

    [FIX] add redirect in layouts

--- src/layouts/DashboardLayout.jsx
+++ src/layouts/DashboardLayout.jsx
@@ -1,11 +1,22 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 
 import { Outlet } from 'react-router-dom'
+import { useNavigate } from 'react-router-dom'
+import { useLocation } from 'react-router-dom'
 
 import Navbar from '@components/Navbar'
 import Sidebar from '@components/Sidebar'
 
 const DashboardLayout = () => {
+    const navigate = useNavigate()
+    const {pathname} = useLocation()
+
+    useEffect(()=>{
+        if (pathname === "/dashboard/") {
+            navigate("/dashboard/approval")
+        }
+    },[])
+
     return (
         <div className='flex'>
             <Sidebar />
