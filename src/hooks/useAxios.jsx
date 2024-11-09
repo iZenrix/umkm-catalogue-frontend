a04e16bfcb48765c4ad51af302049be82a285ab0
QousulHaq <aqousulhaq@gmail.com>
2024-11-09 16:35:09 +0700

    [UPDATE-MINOR] update filed in login form

--- src/hooks/useAxios.jsx
+++ src/hooks/useAxios.jsx
@@ -3,27 +3,20 @@ import axios from 'axios';
 
 axios.defaults.baseURL = 'https://umkm-catalogue-backend.vercel.app/';
 
-/**
- fixed :
-  - no need to JSON.stringify to then immediatly do a JSON.parse
-  - don't use export defaults, because default imports are hard to search for
-  - axios already support generic request in one parameter, no need to call specialized ones
-**/
-
 export const useAxios = (axiosParams) => {
     const [response, setResponse] = useState(undefined);
     const [error, setError] = useState('');
     const [loading, setLoading] = useState(true);
 
     const fetchData = async (params) => {
-      try {
-       const result = await axios.request(params);
-       setResponse(result.data);
-       } catch( error ) {
-         setError(error);
-       } finally {
-         setLoading(false);
-       }
+        try {
+            const result = await axios.request(params);
+            setResponse(result.data);
+        } catch (error) {
+            setError(error);
+        } finally {
+            setLoading(false);
+        }
     };
 
     useEffect(() => {
--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -5,7 +5,6 @@ import { useAuth } from '@contexts/AuthContext';
 import { useNavigate } from 'react-router-dom';
 
 const Login = () => {
-    const [username, setUsername] = useState('');
     const [email, setEmail] = useState('');
     const [password, setPassword] = useState('');
     const [error, setError] = useState('');
@@ -17,13 +16,11 @@ const Login = () => {
 
     const handleSubmit = (e) => {
         e.preventDefault();
-        // Validasi form (misalnya: email dan password tidak boleh kosong)
         if (!email || !password) {
             setError('Email and password are required');
         } else {
             setError('');
             setUser({
-                name : username,
                 email : email,
             })
             setIsLogged(true);
@@ -46,15 +43,6 @@ const Login = () => {
                 >
                     <img src="/img/logo.svg" alt="" className='mb-10 w-40'/>
                     <form onSubmit={handleSubmit} style={{ width: '100%' }}>
-                        <TextField
-                            label="Username"
-                            variant="outlined"
-                            fullWidth
-                            margin="normal"
-                            value={username}
-                            onChange={(e) => setUsername(e.target.value)}
-                            required
-                        />
                         <TextField
                             label="Email"
                             variant="outlined"
