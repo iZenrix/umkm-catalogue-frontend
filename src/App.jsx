4e8bf2fc2ee4fad315da72447436f00760e5ca83
iZenrix <fajrulf960@gmail.com>
2024-11-15 22:13:56 +0700

    [FIX] index case

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import './App.css'
 import { RouterProvider } from 'react-router-dom'
-import router from '@routers/Index'
+import router from '@routers/index'
 import { AuthProvider } from './contexts/AuthContext'
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
